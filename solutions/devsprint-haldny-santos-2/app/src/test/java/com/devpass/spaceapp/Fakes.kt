b21b19b0a88cec30ead0a9f49448864c934466d9
vitoriaseabra <vitoriaseabra_@hotmail.com>
2022-09-29 17:47:53 -0300

    Implement LaunchpadDetailRepositoryImpl unit test

--- solutions/devsprint-haldny-santos-2/app/src/test/java/com/devpass/spaceapp/Fakes.kt
+++ solutions/devsprint-haldny-santos-2/app/src/test/java/com/devpass/spaceapp/Fakes.kt
@@ -6,7 +6,10 @@ import com.devpass.spaceapp.data.api.response.Links
 import com.devpass.spaceapp.data.api.response.OptionsRequest
 import com.devpass.spaceapp.data.api.response.Patch
 import com.devpass.spaceapp.data.api.response.QueryParams
+import com.devpass.spaceapp.data.api.response.LaunchpadDetailResponse
 import com.devpass.spaceapp.model.Launch
+import com.devpass.spaceapp.model.LaunchpadDetail
+
 
 val fakeQueryParams = QueryParams(OptionsRequest(20))
 
@@ -44,4 +47,26 @@ val fakeLaunch = Launch(
     rocketId = "",
     details = "",
     launchpadId = ""
+)
+
+val fakeLaunchpadDetailResponse = LaunchpadDetailResponse(
+    id = "id",
+    name = "VAFB SLC 4E",
+    locality = "Vandenberg Air Force Base",
+    region = "California",
+    latitude = 34.74203,
+    longitude = -120.57244,
+    launchAttempts = 15,
+    launchSuccesses = 15
+)
+
+val fakeLaunchpadDetail = LaunchpadDetail(
+    id = "id",
+    name = "VAFB SLC 4E",
+    locality = "Vandenberg Air Force Base",
+    region = "California",
+    latitude = 34.74203,
+    longitude = -120.57244,
+    launchAttempts = 15,
+    launchSuccesses = 15
 )
