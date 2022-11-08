5a2722a89ee9ed4ee6ca675ed16f4ce658747f78
feandrade <feandrade@brq.com>
2022-11-08 13:44:17 -0300

    Adjustment Test

--- solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/launches/LaunchModelMapperImplTest.kt
+++ solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/launches/LaunchModelMapperImplTest.kt
@@ -7,15 +7,9 @@ import com.devpass.spaceapp.model.Launch
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.util.*
 
 class LaunchModelMapperImplTest {
     private lateinit var launchModelMapperImpl : LaunchModelMapperImpl
-//    val timestamp = "1666817319"
-    val date = Date.from(Instant.now())
-    val sdf = SimpleDateFormat("MMMM dd, yyyy")
 
     @Before
     fun setUp() {
@@ -24,53 +18,68 @@ class LaunchModelMapperImplTest {
 
     @Test
     fun `test Transforms Launches Response To Launch Success` (){
-        val launchModelMock = launchModelMock()
-        launchModelMapperImpl.transformToLaunchModel(launchesResponseMockTrue()).let {
+        val launchResponse = launchesResponseMockTrue()
+        launchModelMapperImpl.transformToLaunchModel(launchResponse).let {
 
-            Assert.assertEquals(launchModelMock, it)
+            Assert.assertEquals(launchSuccess(), it)
         }
     }
 
     @Test
     fun `test Transforms Launches Response To Launch Fail` (){
-        val launchModelMock = launchModelMock()
-        launchModelMapperImpl.transformToLaunchModel(launchesResponseMockFalse()).let {
+        val launchesResponse = launchesResponseMockFalse()
+        launchModelMapperImpl.transformToLaunchModel(launchesResponse).let {
 
-            Assert.assertNotEquals(launchModelMock, it)
+            Assert.assertEquals(launchFail(), it)
         }
     }
 
     private fun launchesResponseMockTrue() = LaunchesResponse(
-        links = Links(Patch("")),
-        nameRocket = "",
-        date = "",
+        links = Links(
+            patch = Patch(small = "https://images2.imgbox.com/94/f2/NN6Ph45r_o.png")
+        ),
+        nameRocket = "FalconSat",
+        date = "2006-03-24T22:30:00.000Z",
         status = true,
         flightNumber = 1,
-        rocketId = "",
-        details = "",
-        launchpadId = ""
+        rocketId = "5e9d0d95eda69955f709d1eb",
+        details = "Engine failure at 33 seconds and loss of vehicle",
+        launchpadId = "5e9e4502f5090995de566f86"
     )
 
-    private fun launchModelMock() = Launch(
-        name= "",
-        number = "1",
-        date = sdf.format(date),
-        status = "Success",
-        image = "",
-        rocketId = "",
-        details = "",
-        launchpadId = ""
-    )
+    private fun launchSuccess() = Launch(
+            name="FalconSat",
+            number= "1",
+            date="março 24, 2006",
+            status="Success",
+            image="https://images2.imgbox.com/94/f2/NN6Ph45r_o.png",
+            rocketId="5e9d0d95eda69955f709d1eb",
+            details="Engine failure at 33 seconds and loss of vehicle",
+            launchpadId="5e9e4502f5090995de566f86"
+        )
+
+    private fun launchFail() = Launch(
+            name="FalconSat",
+            number= "1",
+            date="março 24, 2006",
+            status="Fail",
+            image="https://images2.imgbox.com/94/f2/NN6Ph45r_o.png",
+            rocketId="5e9d0d95eda69955f709d1eb",
+            details="Engine failure at 33 seconds and loss of vehicle",
+            launchpadId="5e9e4502f5090995de566f86"
+        )
 
     private fun launchesResponseMockFalse() = LaunchesResponse(
-        links = Links(Patch("")),
-        nameRocket = "",
-        date = "",
+        links = Links(
+            patch = Patch(small = "https://images2.imgbox.com/94/f2/NN6Ph45r_o.png")
+        ),
+        nameRocket = "FalconSat",
+        date = "2006-03-24T22:30:00.000Z",
         status = false,
-        flightNumber = 0,
-        rocketId = "",
-        details = "",
-        launchpadId = ""
+        flightNumber = 1,
+        rocketId = "5e9d0d95eda69955f709d1eb",
+        details = "Engine failure at 33 seconds and loss of vehicle",
+        launchpadId = "5e9e4502f5090995de566f86"
     )
 
 
