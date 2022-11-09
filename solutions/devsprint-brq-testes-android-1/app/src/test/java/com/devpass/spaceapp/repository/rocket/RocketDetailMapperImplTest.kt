249f7830028d68db7d13a9ad92d1155e342c9573
Gideon Marques <gideonmarques@brq.com>
2022-11-09 11:11:25 -0300

    Criado teste unitário para a class RocketDetailMapperImpl

---
+++ solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/rocket/RocketDetailMapperImplTest.kt
@@ -0,0 +1,41 @@
+package com.devpass.spaceapp.repository.rocket
+
+import com.devpass.spaceapp.data.api.response.RocketDetailResponse
+import com.devpass.spaceapp.model.RocketDetail
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+
+class RocketDetailMapperImplTest {
+
+    private val mapper: RocketDetailMapperImpl = mockk()
+
+    private val rocketDetailResponse = RocketDetailResponse(
+        id = "id",
+        name = "name",
+        description = "description",
+        flickrImages = listOf("image_url")
+    )
+
+    private val rocketDetail = RocketDetail(
+        id = "id",
+        name = "name",
+        description = "description",
+        image = "image_url"
+    )
+
+
+    @Test
+    fun `transform the rocket detailed response model into the rocket model`() {
+        //Given
+        every { mapper.transformToRocketModel(rocketDetailResponse) } returns rocketDetail
+
+        //When
+        val result = mapper.transformToRocketModel(rocketDetailResponse)
+
+        //Then
+        Assert.assertSame(rocketDetail, result)
+
+    }
+}
