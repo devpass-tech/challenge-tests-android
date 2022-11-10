5b961e7edaabf46bbb5d8411de4dbbc9873d64f6
amanddaluz <amandaluz@brq.com>
2022-11-10 20:14:36 -0300

    [GOTO] test LaunchpadDetailMapperImplTest

--- solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/launchpad/LaunchpadDetailMapperImplTest.kt
+++ solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/launchpad/LaunchpadDetailMapperImplTest.kt
@@ -2,47 +2,30 @@ package com.devpass.spaceapp.repository.launchpad
 
 import com.devpass.spaceapp.data.api.response.LaunchpadDetailResponse
 import com.devpass.spaceapp.model.LaunchpadDetail
+import io.mockk.mockk
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Test
 
 class LaunchpadDetailMapperImplTest {
 
     private lateinit var launchpadDetailMapperImpl : LaunchpadDetailMapperImpl
 
-    @Before
-    fun setUp() {
-        launchpadDetailMapperImpl = LaunchpadDetailMapperImpl()
-    }
-
     @Test
     fun `test transform Response to Domain success` (){
+        launchpadDetailMapperImpl = LaunchpadDetailMapperImpl()
         val expected = LaunchpadDetailMock()
         launchpadDetailMapperImpl.transformToLaunchpadModel(LaunchpadDetailResponseMock()).let {
             Assert.assertEquals(expected, it)
         }
     }
 
-    private fun LaunchpadDetailMock() = LaunchpadDetail(
-        id = "",
-        name = "",
-        locality = "",
-        region = "",
-        latitude = 0.0,
-        longitude = 0.0,
-        launchAttempts = 0,
-        launchSuccesses = 0
-    )
+    private fun LaunchpadDetailMock(): LaunchpadDetail {
+        val launchpadDetailData = mockk<LaunchpadDetail>()
+        return launchpadDetailData
+    }
 
-    private fun LaunchpadDetailResponseMock() = LaunchpadDetailResponse(
-        id = "",
-        name = "",
-        locality = "",
-        region = "",
-        latitude = 0.0,
-        longitude = 0.0,
-        launchAttempts = 0,
-        launchSuccesses = 0
-    )
-
+    private fun LaunchpadDetailResponseMock(): LaunchpadDetailResponse {
+        val launchpadDetailResponseData = mockk<LaunchpadDetailResponse>()
+        return launchpadDetailResponseData
+    }
 }
