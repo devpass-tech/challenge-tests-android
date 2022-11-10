e9833f70ebc8cd30d23ba0db72427febc8ac95a2
David Castro <david.dev.designer@gmail.com>
2022-11-10 15:37:04 -0300

    FetchLaunchesRepositoryImplTest

--- solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/launches/FetchLaunchesRepositoryImplTest.kt
+++ solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/launches/FetchLaunchesRepositoryImplTest.kt
@@ -8,6 +8,7 @@ import com.devpass.spaceapp.data.api.response.Patch
 import com.devpass.spaceapp.model.Launch
 import com.devpass.spaceapp.utils.NetworkResult
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.junit4.MockKRule
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -21,27 +22,22 @@ import org.junit.Test
 class FetchLaunchesRepositoryImplTest {
 
     @get:Rule
-    val mockkRule = MockKRule(this)
-
-    @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var api: SpaceXAPIService
-    private lateinit var mapper: LaunchModelMapper
-    private lateinit var repository: FetchLaunchesRepository
-
-    @Before
-    fun setup() {
-        api = mockk(relaxed = true)
-        mapper = LaunchModelMapperImpl()
-        repository = FetchLaunchesRepositoryImpl(api, mapper)
-    }
+    private val api: SpaceXAPIService = mockk(relaxed = true)
+    private val mapper = mockk<LaunchModelMapper>()
+    private val repository = FetchLaunchesRepositoryImpl(api, mapper)
+    private val mockResponse = listOf(mockk<LaunchesResponse>(relaxed = true))
+    private val mockLaunch = mockk<Launch>()
+    private val mockListLaunch = listOf(mockLaunch)
 
     @Test
     fun `when fetch launches result sucess`() = runTest {
         val expected = networkResultSucess()
 
-        coEvery { api.fetchNextLaunches(any()).docs } returns mockResponse()
+        coEvery { api.fetchNextLaunches(any()).docs } returns mockResponse
+
+        every { mapper.transformToLaunchModel(any()) } returns mockLaunch
 
         val result = fetchLaunches()
 
@@ -52,7 +48,7 @@ class FetchLaunchesRepositoryImplTest {
     fun `when fetch launches result has error`() = runTest {
         val expected = networkResultError()
 
-        coEvery { repository.fetchLaunches() } throws Exception()
+        coEvery { fetchLaunches() } throws Exception()
 
         val result = fetchLaunches()
 
@@ -63,38 +59,8 @@ class FetchLaunchesRepositoryImplTest {
         repository.fetchLaunches()
 
     private fun networkResultSucess(): NetworkResult.Success<List<Launch>> =
-        NetworkResult.Success(mockListLaunch())
+        NetworkResult.Success(mockListLaunch)
 
     private fun networkResultError(): NetworkResult.Error<Nothing> =
         NetworkResult.Error(Exception())
-
-    private fun mockListLaunch() = listOf(
-        Launch(
-            name = "DemoSat",
-            number = "2",
-            date = "março 20, 2007",
-            status = "Fail",
-            image = "https://images2.imgbox.com/f9/4a/ZboXReNb_o.png",
-            rocketId = "5e9d0d95eda69955f709d1eb",
-            details = "Successful first stage burn and transition to second stage, maximum altitude 289 km, Premature engine shutdown at T+7 min 30 s, Failed to reach orbit, Failed to recover first stage",
-            launchpadId = "5e9e4502f5090995de566f86"
-        )
-    )
-
-    private fun mockResponse() = listOf(
-            LaunchesResponse(
-                links=Links(
-                    patch=Patch(
-                        small="https://images2.imgbox.com/f9/4a/ZboXReNb_o.png"
-                    )
-                ),
-                nameRocket="DemoSat",
-                date="2007-03-21T01:10:00.000Z",
-                status=false,
-                flightNumber=2,
-                rocketId="5e9d0d95eda69955f709d1eb",
-                details="Successful first stage burn and transition to second stage, maximum altitude 289 km, Premature engine shutdown at T+7 min 30 s, Failed to reach orbit, Failed to recover first stage",
-                launchpadId="5e9e4502f5090995de566f86"
-            ),
-        )
 }
