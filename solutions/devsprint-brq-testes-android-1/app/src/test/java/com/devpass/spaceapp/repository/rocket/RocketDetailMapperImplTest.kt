026f8f2e2d01f92d258a539f9ecaf52cf4d0a229
Gideon Marques da Silva <gideonmarquesdasilva@gmail.com>
2022-11-21 14:05:21 -0300

    [GOTO] - Criado teste unitário para a class RocketDetailMapperImpl (#27)
    * Criado teste unitário para a class RocketDetailMapperImpl
    
    * Alteração do tipo de mock do objeto alvo do teste
    
    * Retirando mock do objeto e usando implementação real da classe de modelo
    
    Co-authored-by: Gideon Marques <gideonmarques@brq.com>

---
+++ solutions/devsprint-brq-testes-android-1/app/src/test/java/com/devpass/spaceapp/repository/rocket/RocketDetailMapperImplTest.kt
@@ -0,0 +1,40 @@
+package com.devpass.spaceapp.repository.rocket
+
+import com.devpass.spaceapp.data.api.response.RocketDetailResponse
+import com.devpass.spaceapp.model.RocketDetail
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import org.junit.Assert
+import org.junit.Test
+
+class RocketDetailMapperImplTest {
+
+    private val rocketDetailResponse = RocketDetailResponse(
+        id = "id",
+        name = "name",
+        description = "description",
+        flickrImages = listOf("image_url")
+    )
+
+    private val rocketDetail = RocketDetail(
+        id = "id",
+        name = "name",
+        description = "description",
+        image = "image_url"
+    )
+
+
+    @Test
+    fun `transform the rocket detailed response model into the rocket model`() {
+        //Given
+        val mapper = RocketDetailMapperImpl()
+
+        //When
+        val result = mapper.transformToRocketModel(rocketDetailResponse)
+
+        //Then
+        Assert.assertEquals(rocketDetail, result)
+
+    }
+}
