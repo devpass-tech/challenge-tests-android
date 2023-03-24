e4fda75eb96e536088f6f7ab7f17c22597294d91
Lucas Emboava <emboavalucas@hotmail.com>
2023-03-24 01:22:31 -0300

    fetchLaunchpad Test

---
+++ solutions/devsprint-gabriel-goto-4/app/src/test/java/com/devpass/spaceapp/Fakes.kt
@@ -0,0 +1,26 @@
+package com.devpass.spaceapp
+
+import com.devpass.spaceapp.data.api.response.LaunchpadDetailResponse
+import com.devpass.spaceapp.model.LaunchpadDetail
+
+val fakeLaunchpadDetailResponse = LaunchpadDetailResponse(
+    id = "id",
+    name = "VAFB SLC 4E",
+    locality = "Vandenberg Air Force Base",
+    region = "California",
+    latitude = 34.74203,
+    longitude = -120.57244,
+    launchAttempts = 15,
+    launchSuccesses = 15
+)
+
+val fakeLaunchpadDetail = LaunchpadDetail(
+    id = "id",
+    name = "VAFB SLC 4E",
+    locality = "Vandenberg Air Force Base",
+    region = "California",
+    latitude = 34.74203,
+    longitude = -120.57244,
+    launchAttempts = 15,
+    launchSuccesses = 15
+)
---
+++ solutions/devsprint-gabriel-goto-4/app/src/test/java/com/devpass/spaceapp/repository/launchpad/LaunchpadDetailRepositoryImplTest.kt
@@ -0,0 +1,72 @@
+package com.devpass.spaceapp.repository.launchpad
+
+import com.devpass.spaceapp.data.api.SpaceXAPIService
+import com.devpass.spaceapp.fakeLaunchpadDetail
+import com.devpass.spaceapp.fakeLaunchpadDetailResponse
+import com.devpass.spaceapp.utils.NetworkResult
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class LaunchpadDetailRepositoryImplTest {
+
+    private val api = mockk<SpaceXAPIService>(relaxed = true)
+    private val mapper = mockk<LaunchpadDetailMapper>(relaxed = true)
+    private val repository = LaunchpadDetailRepositoryImpl(api, mapper)
+
+    @Test
+    fun `GIVEN LaunchpadDetailResponse WHEN fetchLaunchpad is called THEN return a NetworkResult success`() {
+        runTest {
+
+            //GIVEN
+            val launchpadDetailResponse = fakeLaunchpadDetailResponse
+            val launchpadDetail = fakeLaunchpadDetail
+
+            every {
+                mapper.transformToLaunchpadModel(launchpadDetailResponse)
+            } returns launchpadDetail
+
+            val expected = NetworkResult.Success(launchpadDetail)
+
+            coEvery {
+                api.fetchLaunchpadDetails(ID)
+            } returns launchpadDetailResponse
+
+            //WHEN
+            val result = repository.fetchLaunchpad(ID)
+
+            //THEN
+            coVerify { api.fetchLaunchpadDetails(ID) }
+            TestCase.assertEquals(expected, result)
+
+        }
+
+    }
+
+    @Test(expected = Exception::class)
+    fun `GIVEN LaunchpadDetailResponse WHEN fetchLaunchpad is called THEN return a NetworkResult error`() {
+        runTest {
+            //GIVEN
+            val expected = NetworkResult.Error<Any>(RuntimeException())
+
+            coEvery {
+                api.fetchLaunchpadDetails(ID)
+            } throws RuntimeException()
+
+            //WHEN
+            val result = repository.fetchLaunchpad(ID)
+
+            //THEN
+            coVerify { api.fetchLaunchpadDetails(ID) }
+            TestCase.assertEquals(expected, result)
+        }
+    }
+
+    companion object {
+        const val ID = "ANY_ID"
+    }
+}
