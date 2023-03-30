9321e5cd0cb37282321ba05369f019b852b6a1f1
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2023-03-30 18:50:19 -0300

    Feature/test launch list viewmodel (#59)
    * Implementado teste para o metodo safeLaunchesCall
    
    * Implementado teste para o metodo transformToLaunchModel

--- solutions/devsprint-gabriel-goto-4/app/src/test/java/com/devpass/spaceapp/presentation/launch_list/LaunchListViewModelTest.kt
+++ solutions/devsprint-gabriel-goto-4/app/src/test/java/com/devpass/spaceapp/presentation/launch_list/LaunchListViewModelTest.kt
@@ -2,6 +2,7 @@ package com.devpass.spaceapp.presentation.launch_list
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.devpass.spaceapp.model.Launch
 import com.devpass.spaceapp.repository.launches.FetchLaunchesRepository
 import com.devpass.spaceapp.rules.MainDispatcherRule
 import com.devpass.spaceapp.utils.NetworkResult
@@ -14,6 +15,7 @@ import io.mockk.verifySequence
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.io.IOException
 
 class LaunchListViewModelTest {
 
@@ -29,6 +31,8 @@ class LaunchListViewModelTest {
     val observer = mockk<Observer<in LaunchListViewModel.LaunchListUIState>>()
 
     val error = Exception()
+    val networkSuccess = mockk<List<Launch>>(relaxed = true)
+    val networkError = IOException()
 
     @Before
     fun setup() {
@@ -44,6 +48,47 @@ class LaunchListViewModelTest {
     fun `GIVEN launchList WHEN network error THEN return ui error state`() {
         // Arrange.
         val expected = LaunchListViewModel.LaunchListUIState.Error(error)
+
+        // Act.
+        subject.getLaunches()
+        // Advanced coroutines here until finish
+        mainDispatcherRule.dispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        verifySequence {
+            observer.onChanged(LaunchListViewModel.LaunchListUIState.Loading)
+            observer.onChanged(expected)
+            observer.onChanged(LaunchListViewModel.LaunchListUIState.Loading)
+            observer.onChanged(expected)
+        }
+
+    }
+
+    @Test
+    fun `GIVEN launchList WHEN network success THEN return ui success state`() {
+        // Arrange.
+        coEvery { repository.fetchLaunches() } returns NetworkResult.Success(networkSuccess)
+        val expected = LaunchListViewModel.LaunchListUIState.Success(networkSuccess)
+
+        // Act.
+        subject.getLaunches()
+        // Advanced coroutines here until finish
+        mainDispatcherRule.dispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        verifySequence {
+            observer.onChanged(LaunchListViewModel.LaunchListUIState.Loading)
+            observer.onChanged(expected)
+            observer.onChanged(LaunchListViewModel.LaunchListUIState.Loading)
+            observer.onChanged(expected)
+        }
+    }
+
+    @Test
+    fun `GIVEN launchList WHEN network failure THEN return ui error state`() {
+        // Arrange.
+        coEvery{repository.fetchLaunches()} throws networkError
+        val expected = LaunchListViewModel.LaunchListUIState.Error(networkError)
 
         // Act.
         subject.getLaunches()
